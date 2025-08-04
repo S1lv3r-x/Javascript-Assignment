f8c39c9e3dfa38045902bdd0f32458940a3010b2
Silverx9_ <silverx9sung@gmail.com>
2025-08-04 08:23:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-const rectangle = document.getElementById("rectangle");
+    const rectangle = document.getElementById("rectangle");
     const button = document.getElementById("moveBtn");
 
     const corners = [
-      { bottom: "20px", left: "20px" },   
-      { bottom: "20px", right: "20px" },  
-      { top: "20px", right: "20px" },    
-      { top: "20px", left: "20px" }   
+      { bottom: "20px", left: "20px" },
+      { bottom: "20px", right: "20px" },
+      { top: "20px", right: "20px" },
+      { top: "20px", left: "20px" }
     ];
 
     const colors = ["red", "orange", "yellow", "pink"];
-
     let currentCorner = 0;
+
     function setCorner(index) {
       rectangle.style.top = "";
       rectangle.style.bottom = "";
       rectangle.style.left = "";
       rectangle.style.right = "";
 
-      const pos = corners[index];
-      for (let key in pos) {
-        rectangle.style[key] = pos[key];
-      }
-
-      rectangle.style.backgroundColor = colors[index];
+      Object.assign(rectangle.style, corners[index]);
+      rectangle.style.background = colors[index];
     }
 
     setCorner(currentCorner);
-    button.addEventListener("click", () => {
+    button.onclick = () => {
       currentCorner = (currentCorner + 1) % corners.length;
       setCorner(currentCorner);
-    });
+    };
